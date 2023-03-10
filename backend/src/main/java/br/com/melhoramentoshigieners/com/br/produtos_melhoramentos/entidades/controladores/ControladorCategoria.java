139a520757174a56512011e9676ed725fef66569
Rafael de Luca <deluca1712@gmail.com>
2023-03-10 12:15:34 -0300

    Entidade Categoria

---
+++ backend/src/main/java/br/com/melhoramentoshigieners/com/br/produtos_melhoramentos/entidades/controladores/ControladorCategoria.java
@@ -0,0 +1,33 @@
+package br.com.melhoramentoshigieners.com.br.produtos_melhoramentos.entidades.controladores;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.melhoramentoshigieners.com.br.produtos_melhoramentos.entidades.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class ControladorCategoria {
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> buscarTodas () {
+		List<Categoria> listDeCategorias = new ArrayList<Categoria>();
+		
+		listDeCategorias.add(new Categoria(1L, "Papel Toalha"));
+		listDeCategorias.add(new Categoria(2L, "Papel Higiênico"));
+		listDeCategorias.add(new Categoria(2L, "Guardanapo"));
+		listDeCategorias.add(new Categoria(3L, "Interfolhado"));
+		listDeCategorias.add(new Categoria(4L, "Rolo"));
+		listDeCategorias.add(new Categoria(5L, "Excellence"));
+		listDeCategorias.add(new Categoria(6L, "Plus"));
+		listDeCategorias.add(new Categoria(7L, "Classic"));
+		
+		return ResponseEntity.ok().body(listDeCategorias);
+	}
+
+}
