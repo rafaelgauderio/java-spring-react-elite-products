867ab29ed5807c3f20cd00bab04c6e8e70b85d2a
Rafael de Luca <deluca1712@gmail.com>
2023-03-15 12:04:27 -0300

    Exceçãode violação de integridade de banco de dados

--- backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/controladores/excecoes/ManipuladorDeExcecoesDoControlador.java
+++ backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/controladores/excecoes/ManipuladorDeExcecoesDoControlador.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import br.com.melhoramentoshigieners.produtos_melhoramentos.servicos.excecoes.ExcecaoEntidadeNaoEncontrada;
+import br.com.melhoramentoshigieners.produtos_melhoramentos.servicos.excecoes.ExcecaoIntegridadeBancoDeDados;
 import jakarta.servlet.http.HttpServletRequest;
 
 
@@ -19,14 +20,30 @@ public class ManipuladorDeExcecoesDoControlador {
 	@ExceptionHandler(ExcecaoEntidadeNaoEncontrada.class)
 	public ResponseEntity<ExcecaoCustomizada> excecaoEntidadeNaoEncontrada(ExcecaoEntidadeNaoEncontrada mensagemErro, HttpServletRequest requisicao) {
 		
+		HttpStatus codigoHttp = HttpStatus.NOT_FOUND;
 		ExcecaoCustomizada excecaoCustomizada = new ExcecaoCustomizada();
 		excecaoCustomizada.setNomeDoErro("Recurso não Encontrado");
 		excecaoCustomizada.setMenssagemDeErro(mensagemErro.getMessage());
 		excecaoCustomizada.setCaminho(requisicao.getRequestURI());
-		excecaoCustomizada.setCodigoHttpDoErro(HttpStatus.NOT_FOUND.value()); // CÓDIGO HTTP 404
+		excecaoCustomizada.setCodigoHttpDoErro(codigoHttp.value()); // CÓDIGO HTTP 404
 		excecaoCustomizada.setMomentoDoErro(Instant.now());
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(excecaoCustomizada);		
+		return ResponseEntity.status(codigoHttp).body(excecaoCustomizada);		
+	}
+	
+	@ExceptionHandler(ExcecaoIntegridadeBancoDeDados.class)
+	public ResponseEntity<ExcecaoCustomizada> excecaoDeBancoDeDados (ExcecaoIntegridadeBancoDeDados mensagemErro, HttpServletRequest requisicao) {
+		
+		HttpStatus codigoHttp = HttpStatus.FORBIDDEN;
+		ExcecaoCustomizada excecaoCustomizada = new ExcecaoCustomizada();
+		excecaoCustomizada.setNomeDoErro("Exceção de banco de dados");
+		excecaoCustomizada.setMenssagemDeErro(mensagemErro.getMessage());
+		excecaoCustomizada.setCaminho(requisicao.getRequestURI());
+		// não pode excluir um entidade que relaciona com outra entidade. vai dar erro de violação de integridade de database
+		excecaoCustomizada.setCodigoHttpDoErro(codigoHttp.value()); // CÓDIGO HTTP 403
+		excecaoCustomizada.setMomentoDoErro(Instant.now());
+		
+		return ResponseEntity.status(codigoHttp).body(excecaoCustomizada);		
 	}
 
 }
