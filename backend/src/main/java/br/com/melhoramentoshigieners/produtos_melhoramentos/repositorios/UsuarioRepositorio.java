0e04291d61d414e034618fa4397416f73ff9db03
Rafael De Luca <deluca1712@gmail.com>
2023-03-26 20:38:31 -0300

    Reposirios de Usuario e Regra

---
+++ backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/repositorios/UsuarioRepositorio.java
@@ -0,0 +1,9 @@
+package br.com.melhoramentoshigieners.produtos_melhoramentos.repositorios;
+
+import br.com.melhoramentoshigieners.produtos_melhoramentos.entidades.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+    @Repository
+    public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
+}
