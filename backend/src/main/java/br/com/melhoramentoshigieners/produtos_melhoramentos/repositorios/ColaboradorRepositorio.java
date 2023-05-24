da68946eff5d6197972de663307db53efc49ad5d
Rafael de Luca <deluca1712@gmail.com>
2023-05-24 14:19:43 -0300

    Repositorios de Colaborador e Departamento

---
+++ backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/repositorios/ColaboradorRepositorio.java
@@ -0,0 +1,9 @@
+package br.com.melhoramentoshigieners.produtos_melhoramentos.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ColaboradorRepositorio extends JpaRepository {
+	
+}
