f07b0a30609eb958c782150e7bc255ad8f2a5fc9
Rafael de Luca <deluca1712@gmail.com>
2023-05-25 11:42:58 -0300

    Metodo deletar um produto

--- backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/controladores/DepartamentoControlador.java
+++ backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/controladores/DepartamentoControlador.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,34 +21,42 @@ import br.com.melhoramentoshigieners.produtos_melhoramentos.servicos.Departament
 @RestController
 @RequestMapping(value = "departamentos")
 public class DepartamentoControlador {
-	
+
 	@Autowired
 	private DepartamentoService service;
-	
-	@GetMapping 
-	public ResponseEntity<List<DepartamentoDTO>> buscarTodosDepartamentos () {
+
+	@GetMapping
+	public ResponseEntity<List<DepartamentoDTO>> buscarTodosDepartamentos() {
 		List<DepartamentoDTO> listaDepartamentoDTO = service.findAll();
-		return ResponseEntity.ok().body(listaDepartamentoDTO);		
+		return ResponseEntity.ok().body(listaDepartamentoDTO);
 	}
-	
-	@GetMapping(value="/{id}")
-	public ResponseEntity<DepartamentoDTO> buscarDepartamentoPorId (@PathVariable Long id) {
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<DepartamentoDTO> buscarDepartamentoPorId(@PathVariable Long id) {
 		DepartamentoDTO departamentoDTO = service.findById(id);
 		return ResponseEntity.ok().body(departamentoDTO);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<DepartamentoDTO> inserirDepartamento (@RequestBody DepartamentoDTO departamentoDTO) {
+	public ResponseEntity<DepartamentoDTO> inserirDepartamento(@RequestBody DepartamentoDTO departamentoDTO) {
 		departamentoDTO = service.insert(departamentoDTO);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(departamentoDTO.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(departamentoDTO.getId())
+				.toUri();
 		return ResponseEntity.created(uri).body(departamentoDTO);
-		
+
 	}
-	
-	@PutMapping(value="/{id}")
-	public ResponseEntity<DepartamentoDTO> atualizarDepartamento(@PathVariable Long id, @RequestBody DepartamentoDTO dto) {
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<DepartamentoDTO> atualizarDepartamento(@PathVariable Long id,
+			@RequestBody DepartamentoDTO dto) {
 		dto = service.update(id, dto);
-		return ResponseEntity.ok().body(dto);		
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleterDepartamento(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/servicos/DepartamentoService.java
+++ backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/servicos/DepartamentoService.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,48 +16,62 @@ import br.com.melhoramentoshigieners.produtos_melhoramentos.dto.DepartamentoDTO;
 import br.com.melhoramentoshigieners.produtos_melhoramentos.entidades.Departamento;
 import br.com.melhoramentoshigieners.produtos_melhoramentos.repositorios.DepartamentoRepositorio;
 import br.com.melhoramentoshigieners.produtos_melhoramentos.servicos.excecoes.ExcecaoEntidadeNaoEncontrada;
+import br.com.melhoramentoshigieners.produtos_melhoramentos.servicos.excecoes.ExcecaoIntegridadeBancoDeDados;
 
 @Service
 public class DepartamentoService {
-	
+
 	@Autowired
 	private DepartamentoRepositorio repositorio;
-	
-	@Transactional(readOnly=true)
-	public List<DepartamentoDTO> findAll () {
-		
+
+	@Transactional(readOnly = true)
+	public List<DepartamentoDTO> findAll() {
+
 		List<Departamento> listaDepartamento = repositorio.findAll();
 		// convertendo de lista para map, aplicando um função e reconvertendo para lista
-		return listaDepartamento.stream().map(dep -> new DepartamentoDTO(dep)).collect(Collectors.toList());		
+		return listaDepartamento.stream().map(dep -> new DepartamentoDTO(dep)).collect(Collectors.toList());
 	}
-	
-	@Transactional(readOnly=true)
-	public DepartamentoDTO findById (Long id) {
+
+	@Transactional(readOnly = true)
+	public DepartamentoDTO findById(Long id) {
 		Optional<Departamento> optinal = repositorio.findById(id);
-		Departamento departamento = optinal.orElseThrow( ( )-> new ExcecaoEntidadeNaoEncontrada("Departamento não encontrado com a id informada!"));
+		Departamento departamento = optinal
+				.orElseThrow(() -> new ExcecaoEntidadeNaoEncontrada("Departamento não encontrado com a id informada!"));
 		return new DepartamentoDTO(departamento);
 	}
-	
-	@Transactional(readOnly=false)
-	public DepartamentoDTO insert (DepartamentoDTO dto) {
-		Departamento entidade = new Departamento ();
+
+	@Transactional(readOnly = false)
+	public DepartamentoDTO insert(DepartamentoDTO dto) {
+		Departamento entidade = new Departamento();
 		entidade.setNome(dto.getNome());
 		entidade = repositorio.save(entidade);
 		return new DepartamentoDTO(entidade);
 	}
-	
-	@Transactional(readOnly=false)
+
+	@Transactional(readOnly = false)
 	public DepartamentoDTO update(Long id, DepartamentoDTO dto) {
-		
+
 		try {
 			Departamento entidade = repositorio.getReferenceById(id);
 			entidade.setNome(dto.getNome());
 			entidade = repositorio.save(entidade);
 			return new DepartamentoDTO(entidade);
 		} catch (EntityNotFoundException erro) {
-			
-			throw new ExcecaoEntidadeNaoEncontrada("Departamento não encontrada com a id de número: " + id);
+
+			throw new ExcecaoEntidadeNaoEncontrada("Departamento não encontrado com id de número: " + id);
 		}
+	}
+
+	public void delete(Long departamentoId) {
+		try {
+			repositorio.deleteById(departamentoId);
+		} catch (EmptyResultDataAccessException erro) {
+			throw new ExcecaoEntidadeNaoEncontrada("Departamento não encontrado com id de número: " + departamentoId);
+		} catch (DataIntegrityViolationException erro) {
+			throw new ExcecaoIntegridadeBancoDeDados(
+					"Não é possivel excluir um departamento que já tem um colaborador cadastrado.");
+		}
+
 	}
 
 }
