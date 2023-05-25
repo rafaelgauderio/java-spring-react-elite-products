91b8fc0939bc1475bc19ef2d54746d42520affed
Rafael de Luca <deluca1712@gmail.com>
2023-05-25 15:57:46 -0300

    Método buscar colaborador por Id

--- backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/controladores/ColaboradorControlador.java
+++ backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/controladores/ColaboradorControlador.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +17,18 @@ import br.com.melhoramentoshigieners.produtos_melhoramentos.servicos.Colaborador
 public class ColaboradorControlador {
 	
 	@Autowired
-	private ColaboradorServico service;
+	private ColaboradorServico servico;
 	
 	@GetMapping
 	public ResponseEntity<List<ColaboradorDTO>> buscarTodosColaboradores() {
-		List<ColaboradorDTO> listColaborador = service.findAll();
+		List<ColaboradorDTO> listColaborador = servico.findAll();
 		return ResponseEntity.ok().body(listColaborador);
-	}	
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<ColaboradorDTO> buscarColaboradorPorId(@PathVariable Long id) {
+		ColaboradorDTO dto = servico.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
 
 }
--- backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/servicos/ColaboradorServico.java
+++ backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/servicos/ColaboradorServico.java
@@ -1,16 +1,17 @@
 package br.com.melhoramentoshigieners.produtos_melhoramentos.servicos;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.melhoramentoshigieners.produtos_melhoramentos.dto.ColaboradorDTO;
 import br.com.melhoramentoshigieners.produtos_melhoramentos.entidades.Colaborador;
 import br.com.melhoramentoshigieners.produtos_melhoramentos.repositorios.ColaboradorRepositorio;
+import br.com.melhoramentoshigieners.produtos_melhoramentos.servicos.excecoes.ExcecaoEntidadeNaoEncontrada;
 
 @Service
 public class ColaboradorServico {
@@ -22,6 +23,13 @@ public class ColaboradorServico {
 	public List<ColaboradorDTO> findAll() {
 		List <Colaborador> listaColaborador = colaboradorRepositorio.findAll();
 		return listaColaborador.stream().map(col -> new ColaboradorDTO(col)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly=true)
+	public ColaboradorDTO findById(Long colaboradorId) {		
+		Optional<Colaborador> optional = colaboradorRepositorio.findById(colaboradorId);
+		Colaborador entidade = optional.orElseThrow(() -> new ExcecaoEntidadeNaoEncontrada("Colaborador não encontrado com id de número: " + colaboradorId));
+		return new ColaboradorDTO(entidade);
 	}
 
 }
