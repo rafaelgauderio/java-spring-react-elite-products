891190db5712158dbc925c945896cfe44e2710bd
Rafael de Luca <deluca1712@gmail.com>
2023-05-26 11:11:29 -0300

    Metodo updade Colaborador

--- backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/controladores/ColaboradorControlador.java
+++ backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/controladores/ColaboradorControlador.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,27 +20,35 @@ import br.com.melhoramentoshigieners.produtos_melhoramentos.servicos.Colaborador
 @RestController
 @RequestMapping(value = "/colaboradores")
 public class ColaboradorControlador {
-	
+
 	@Autowired
 	private ColaboradorServico servico;
-	
+
 	@GetMapping
 	public ResponseEntity<List<ColaboradorDTO>> buscarTodosColaboradores() {
 		List<ColaboradorDTO> listColaborador = servico.findAll();
 		return ResponseEntity.ok().body(listColaborador);
 	}
-	
-	@GetMapping(value="/{id}")
+
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<ColaboradorDTO> buscarColaboradorPorId(@PathVariable Long id) {
 		ColaboradorDTO dto = servico.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<ColaboradorDTO> inserirColaborador(@RequestBody ColaboradorDTO dto){
+	public ResponseEntity<ColaboradorDTO> inserirColaborador(@RequestBody ColaboradorDTO dto) {
 		dto = servico.insert(dto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getDepartamentoId()).toUri();		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getDepartamentoId())
+				.toUri();
 		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ColaboradorDTO> atualizarColaborador(@PathVariable Long id, @RequestBody ColaboradorDTO dto) {
+		dto = servico.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+
 	}
 
 }
--- backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/servicos/ColaboradorServico.java
+++ backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/servicos/ColaboradorServico.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,45 +14,63 @@ import br.com.melhoramentoshigieners.produtos_melhoramentos.dto.ColaboradorDTO;
 import br.com.melhoramentoshigieners.produtos_melhoramentos.entidades.Colaborador;
 import br.com.melhoramentoshigieners.produtos_melhoramentos.entidades.Departamento;
 import br.com.melhoramentoshigieners.produtos_melhoramentos.repositorios.ColaboradorRepositorio;
-import br.com.melhoramentoshigieners.produtos_melhoramentos.repositorios.DepartamentoRepositorio;
 import br.com.melhoramentoshigieners.produtos_melhoramentos.servicos.excecoes.ExcecaoEntidadeNaoEncontrada;
 
 @Service
 public class ColaboradorServico {
-	
+
 	@Autowired
 	private ColaboradorRepositorio colaboradorRepositorio;
-	
-	@Autowired
-	private DepartamentoRepositorio departamentoRepositorio;
-	
-	@Transactional(readOnly=true)
+
+	@Transactional(readOnly = true)
 	public List<ColaboradorDTO> findAll() {
-		List <Colaborador> listaColaborador = colaboradorRepositorio.findAll();
+		List<Colaborador> listaColaborador = colaboradorRepositorio.findAll();
 		return listaColaborador.stream().map(col -> new ColaboradorDTO(col)).collect(Collectors.toList());
 	}
-	
-	@Transactional(readOnly=true)
-	public ColaboradorDTO findById(Long colaboradorId) {		
+
+	@Transactional(readOnly = true)
+	public ColaboradorDTO findById(Long colaboradorId) {
 		Optional<Colaborador> optional = colaboradorRepositorio.findById(colaboradorId);
-		Colaborador entidade = optional.orElseThrow(() -> new ExcecaoEntidadeNaoEncontrada("Colaborador não encontrado com id de número: " + colaboradorId));
+		Colaborador entidade = optional.orElseThrow(() -> new ExcecaoEntidadeNaoEncontrada(
+				"Colaborador não encontrado com id de número: " + colaboradorId));
 		return new ColaboradorDTO(entidade);
 	}
-	
-	@Transactional(readOnly=true)
+
+	@Transactional(readOnly = false)
 	public ColaboradorDTO insert(ColaboradorDTO colaboradorDTO) {
 		Colaborador entidade = new Colaborador();
 		entidade.setNome(colaboradorDTO.getNome());
 		entidade.setEmail(colaboradorDTO.getEmail());
-		entidade.setTelefone(colaboradorDTO.getTelefone());		
-		
+		entidade.setTelefone(colaboradorDTO.getTelefone());
+
 		Departamento departamento = new Departamento();
 		departamento.setId(colaboradorDTO.getDepartamentoId());
 		entidade.setDepartamento(departamento);
-			
+
 		entidade = colaboradorRepositorio.save(entidade);
-		
+
 		return new ColaboradorDTO(entidade);
+	}
+
+	@Transactional(readOnly = false)
+	public ColaboradorDTO update(Long colaboradorId, ColaboradorDTO colaboradorDTO) {
+		try {
+			Colaborador entidade = colaboradorRepositorio.getReferenceById(colaboradorId);
+			entidade.setNome(colaboradorDTO.getNome());
+			entidade.setEmail(colaboradorDTO.getEmail());
+			entidade.setTelefone(colaboradorDTO.getTelefone());
+
+			Departamento departamento = new Departamento();
+			departamento.setId(colaboradorDTO.getDepartamentoId());
+			entidade.setDepartamento(departamento);
+
+			entidade = colaboradorRepositorio.save(entidade);
+			return new ColaboradorDTO(entidade);
+
+		} catch (EntityNotFoundException erro) {
+			throw new ExcecaoEntidadeNaoEncontrada("Colaborador não encontrado com id de número: " + colaboradorId);
+		}
+
 	}
 
 }
