c2755e7f4c8b562e6d4578b4dc794e7d691b58c7
Rafael de Luca <deluca1712@gmail.com>
2023-09-05 11:31:01 -0300

    Testes produto camada de repositorio

---
+++ backend/src/test/java/br/com/melhopramentoshigieners/com/br/produtos_melhoramentos/repositorios/ProdutoRepositorioTests.java
@@ -0,0 +1,78 @@
+package br.com.melhopramentoshigieners.com.br.produtos_melhoramentos.repositorios;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+
+import br.com.melhopramentoshigieners.com.br.produtos_melhoramentos.tests.ProdutoFactory;
+import br.com.melhoramentoshigieners.produtos_melhoramentos.ProdutosMelhoramentosApplication;
+import br.com.melhoramentoshigieners.produtos_melhoramentos.entidades.Produto;
+import br.com.melhoramentoshigieners.produtos_melhoramentos.repositorios.ProdutoRepositorio;
+
+@ContextConfiguration(classes = ProdutosMelhoramentosApplication.class)
+@DataJpaTest
+public class ProdutoRepositorioTests {
+
+    @Autowired
+    private ProdutoRepositorio produtoRepositorio;
+
+    private Long idNaoExistente;
+    private Long idExistente;
+  
+    @BeforeEach
+    void setUp() throws Exception {
+        idExistente = 1L; 
+        idNaoExistente = 200L;
+    }
+    
+    @Test
+	public void findByIdShouldReturnOptionalNotEmptyWhenIdExists () { 
+		Optional<Produto> optinal = produtoRepositorio.findById(idExistente);		
+		Assertions.assertFalse(optinal.isEmpty());
+		Assertions.assertTrue(optinal.isPresent());
+	}
+    
+    @Test
+	public void findByIdShouldReturnOptinalEmptyWhenIdDoesNotExists() {
+	
+		Optional<Produto> optinal = produtoRepositorio.findById(idNaoExistente);		
+		Assertions.assertFalse(optinal.isPresent());
+		Assertions.assertTrue(optinal.isEmpty());
+	}
+
+    @Test
+    public void deleteProdutoShoudlDeteteObjectWhenIdExists() {
+        produtoRepositorio.deleteById(idExistente);
+        Optional<Produto> optional = produtoRepositorio.findById(idExistente);
+        Assertions.assertTrue(optional.isEmpty());
+        Assertions.assertFalse(optional.isPresent());
+    }
+
+    @Test
+    public void deteteProdutoShoudlThrowEmptResultDataEccessExceptionWhenIdDoesNotExists () {
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+           produtoRepositorio.deleteById(idNaoExistente);
+        });
+    }
+    
+    @Test
+    public void saveProductShouldPersistWithAutoIncrementId () {
+    	Produto produto = ProdutoFactory.criarProduto();
+    	produto.setId(null);
+    	
+    	Assertions.assertNull(produto.getId()); 
+    	
+    	produto = produtoRepositorio.save(produto);
+    	Assertions.assertNotNull(produto.getId()); 
+    	Assertions.assertEquals(produtoRepositorio.findAll().size(), produto.getId()); 
+    	Assertions.assertEquals(produto.getCategorias().size(),1);
+    	Assertions.assertEquals(produto.getEmbalagens().size(),1);
+    }      
+    
+}
