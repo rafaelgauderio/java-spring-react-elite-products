4f3ea35e134045fe74fdfac9268c459eb47b94ba
Rafael de Luca <deluca1712@gmail.com>
2023-11-30 14:31:37 -0300

    Testando updload com CommandLineRunner

--- backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/ProdutosMelhoramentosApplication.java
+++ backend/src/main/java/br/com/melhoramentoshigieners/produtos_melhoramentos/ProdutosMelhoramentosApplication.java
@@ -1,13 +1,25 @@
 package br.com.melhoramentoshigieners.produtos_melhoramentos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.melhoramentoshigieners.produtos_melhoramentos.servicos.AwsS3Servico;
+
 @SpringBootApplication
-public class ProdutosMelhoramentosApplication {
+public class ProdutosMelhoramentosApplication implements CommandLineRunner {
+
+	@Autowired
+	private AwsS3Servico awsS3Servico;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosMelhoramentosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {		
+		awsS3Servico.uploadFile("c:\\imagens\\teste.png");
 	}
 
 }
